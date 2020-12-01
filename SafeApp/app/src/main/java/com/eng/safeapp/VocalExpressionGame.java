502190e9569ce1c32565dc96e88246ec079ca82d
Jacobus Burger <therealjacoburger@gmail.com>
2020-12-01 11:31:27 -0800

    Update Documentation for VocalExpressionGame

--- SafeApp/app/src/main/java/com/eng/safeapp/VocalExpressionGame.java
+++ SafeApp/app/src/main/java/com/eng/safeapp/VocalExpressionGame.java
@@ -17,9 +17,56 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/*
+    Author: Jacobus Burger (AKA: LordUbuntu) <therealjacoburger@gmail.com>
+    Last Modified: 2020-12-01
+    Descripion: This class represents the vocal expression game. Which plays voice clips that the
+    player must guess the emotionality of by selecting corresponding emotion buttons.
+
+
+    Data(types/structures):
+        - `MediaPlayer[] voices` collects all voice files into an array that can be accessed with the setVoice method using
+            the emotions enum as a navigational index. This works because the order of its groups corresponds mathematically
+            to the order of entries in the emotions enum.
+        - `Integer[] images` keeps a record of each of the 8 emotion images with the emotions enum acting as a navigaitonal
+            index. This works because the order of its entries corresponds to the order of entries in the emotions enum.
+        - `ImageButton[] buttons` groups the user input buttons into an array for easy manipulation.
+        - `Map<ImageButton, emotions> buttonEmotion` records the mapping of each button to an assigned emotion. This information is
+            then used to deduce what image to assign to each button, and to determine whether the button has the correct emotion
+            corresponding to the answer.
+            
+    Operational Details:
+    VocalExpressionGame uses an `emotions` enum to track which voice files to use, which images
+    to use, and what the correct answer for the round is.
+
+    The OnCreate method is used to setup the initial gamestate. Once called it begins a concurrent
+    timer that will switch the player over to the VocalExpressionGameOver class after it has run
+    for startTime seconds.
+
+    The OnClick method is used to determine what button was pressed and act accordingly.
+        If that button is the voiceButton, then it will replay the voice file.
+        If that button is the quitButton, then it will quit to main menu.
+        If that button is a choice button, then it will check if it is correct.
+            If it is, it will increment the correct score, turn the button green, and move to next round.
+            If it is not, it will increment the incorrect score, turn the button red, and move to next round.
+
+    The setButtons method chooses as many random emotions as there are buttons (with no repeats),
+    recording that association in the buttonEmotion mapping. Then each button is given an image
+    that corresponds with their given emotion, and finally the answer emotion is chosen from among
+    them at random.
+
+    The setVoice method chooses a random voice file that corresponds to the current global answer
+    emotion using the following formula (with the assumption that all of the files in voices
+    are ordered in the same order as in the emotions enum, and that all similar emotions are
+    clumped together into inorder groups):
+            current voice = voices at index random [0, 4) + 4 * emotional offset
+        The formula has a special exception for fear, since it only has 3 files. Due to its special
+        circumstance it must come last in the voices array and be selected with a specialized formula
+        or the program is likely to not work as intended.
+ */
 public class VocalExpressionGame extends AppCompatActivity implements View.OnClickListener {
     // helper values
-    final int startTime = 10; // amount of time to give player in seconds
+    final int startTime = 10; // amount of time to give player for the game in seconds
     Random rand = new Random(); // random number generator
 
     // game state values
@@ -39,15 +86,11 @@ public class VocalExpressionGame extends AppCompatActivity implements View.OnCli
         Fear, // must be last in enum, due to having irregular number of files
     }
 
-    // array of emotion voice acting snippets
-    // note: must correspond to the order of emotions enum
+    // note: must correspond to the order of emotions enum upon initialization
     private MediaPlayer[] voices;
-
-    // note: must correspond to the order of emotions enum
     private Integer[] images;
 
     private ImageButton[] buttons;
-
     Map<ImageButton, emotions> buttonEmotion = new HashMap<ImageButton, emotions>();
 
 
